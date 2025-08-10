83d18c8868948ccd38daf2ccee9f69173cf4ed4c
NICOLAS MARCELO CALUVA <nicolasmarcelo.caluva@alumnos.ucse.edu.ar>
2025-08-10 12:31:35 -0300

    cambio de nombre carpeta principal

--- dondeestudiar/wsgi.py
+++ dondeestudiar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dondeestudiar_ project.
+WSGI config for dondeestudiar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dondeestudiar_.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dondeestudiar.settings')
 
 application = get_wsgi_application()
