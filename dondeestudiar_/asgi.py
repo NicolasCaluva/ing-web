6a78dd8611e19df5ba20cacca3d16833ae969113
NICOLAS MARCELO CALUVA <nicolasmarcelo.caluva@alumnos.ucse.edu.ar>
2025-08-10 12:30:00 -0300

    modificacion de estructura del proyecto

--- dondeestudiar_/asgi.py
+++ dondeestudiar_/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dondeestudiar project.
+ASGI config for dondeestudiar_ project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dondeestudiar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dondeestudiar_.settings')
 
 application = get_asgi_application()
