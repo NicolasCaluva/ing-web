6724d84de625ef29794786a1b7e7f6d5b4750605
Asselborn Gaston <131315561+KGaston0@users.noreply.github.com>
2025-08-12 18:11:51 -0300

    Commit inicial Comments - Comments admin

--- app/comentarios/admin.py
+++ app/comentarios/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Comment
+from .models import Reply
 
 # Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'school')
+    search_fields = ('author', 'text')
+    list_filter = ('author', 'school')
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'school')
+    search_fields = ('author', 'text')
+    list_filter = ('author', 'school')
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Reply, ReplyAdmin)
