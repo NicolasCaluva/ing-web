b49421998aa22ee2584eca4092a38a088ce318b4
Nicolas Caluva <nicolascaluva14@gmail.com>
2025-08-14 00:45:30 -0300

    fix remove redundant url

--- app/schools/urls.py
+++ app/schools/urls.py
@@ -5,5 +5,5 @@ from app.schools.views import school_detail
 app_name = 'schools'
 
 urlpatterns = [
-    path('schools/<int:pk>/', school_detail, name='school_detail'),
+    path('<int:pk>/', school_detail, name='school_detail'),
 ]
