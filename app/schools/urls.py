ecf04686ca2a4beb05c3cdb9895082f1d91b3505
Nicolas Caluva <nicolascaluva14@gmail.com>
2025-08-17 02:34:52 -0300

    Fixed login, added register like school or user

--- app/schools/urls.py
+++ app/schools/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from app.schools.views import school_list, school_detail, delete_comment, edit_comment, edit_reply, delete_reply
+from app.base.views import register_school_view
 
 app_name = 'school'
 
 urlpatterns = [
     path('', school_list, name='school_list'),
+    path('registro/', register_school_view, name='register'),
     path('<int:pk>/', school_detail, name='school_detail'),
     path('<int:pk>/', edit_comment, name='edit_comment'),
     path('<int:pk>/comentario/<int:idComentario>/', delete_comment, name='delete_comment'),
