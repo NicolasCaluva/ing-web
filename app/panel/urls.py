d847bee80c0cd620a33da27f2087ca445fd61eb4
Nicolas Caluva <nicolascaluva14@gmail.com>
2025-08-18 23:11:42 -0300

    added upsert users in admin panel

--- app/panel/urls.py
+++ app/panel/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from app.panel.views import list_users, delete_user
+from app.panel.views import list_users, delete_user, upsert_user
 
 app_name = 'panel'
 
 urlpatterns = [
+    path('', list_users, name='list_users'),
     path('usuarios/', list_users, name='list_users'),
     path('usuarios/eliminar/<int:pk>/', delete_user, name='delete_user'),
+    path('usuarios/crear/', upsert_user, name='create_user'),
+    path('usuarios/editar/<int:pk>/', upsert_user, name='edit_user'),
 ]
--- app/users/models.py
+++ app/users/models.py
@@ -10,6 +10,7 @@ class UserBase(models.Model):
     recovery_code = models.CharField(max_length=10, unique=True, null=True, blank=True)
     email_verified = models.BooleanField(default=False)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
 
