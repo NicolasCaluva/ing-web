f843a836f8a5dd167fea16e34460a03ea15876a5
Nicolas Caluva <nicolascaluva14@gmail.com>
2025-08-30 17:54:02 -0300

    Changed some urls

--- app/comments/urls.py
+++ app/comments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<int:pk>/', comments_view, name='comments_list'),
     path('<int:pk>/', edit_comment, name='edit_comment'),
     path('<int:pk>/comentario/<int:comment_id>/', delete_comment, name='delete_comment'),
-    path('<int:pk>/', edit_reply, name='edit_reply'),
+    path('<int:pk>/reply/edit/', edit_reply, name='edit_reply'),
     path('<int:pk>/comentario/<int:comment_id>/respuesta/<int:reply_id>/', delete_reply, name='delete_reply'),
     path('<int:pk>/comentario/<int:comment_id>/respuesta/', add_reply, name='add_reply'),
 ]
