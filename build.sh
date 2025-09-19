5997755d4c72736a797914e4123c23907d2f6f7d
Nicolas Marcelo Caluva <106607109+NicolasCaluva@users.noreply.github.com>
2025-09-19 18:24:13 -0300

    Add makemigrations command to build script

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cd $(dirname $(find . | grep manage.py$))
 uv run ./manage.py collectstatic --no-input
 
 # aplicar migraciones
+uv run ./manage.py makemigrations
 uv run ./manage.py migrate
 
 # crear superusuario solo si definiste la variable de entorno DJANGO_SUPERUSER_PASSWORD
