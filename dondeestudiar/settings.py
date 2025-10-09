21ee9ebe3f16913313f6dd7023994b4888491f1d
NICOLAS MARCELO CALUVA <nicolasmarcelo.caluva@alumnos.ucse.edu.ar>
2025-10-09 19:07:47 -0300

    pruebita

--- dondeestudiar/settings.py
+++ dondeestudiar/settings.py
@@ -119,7 +119,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # =========================
 if 'RENDER' in os.environ:
     print("⚡ USING RENDER.COM SETTINGS!")
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
     DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
     CLOUDINARY_STORAGE = {
